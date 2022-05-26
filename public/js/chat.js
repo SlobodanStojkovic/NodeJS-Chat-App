0c97cfc36bb84c7c36473209668969bd943569e0
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-26 18:14:18 +0200

    Add message and location templates

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,50 @@
 const socket = io();
 
+//Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+
 socket.on("message", (message) => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+socket.on("locationMessage", (url) => {
+  console.log(url);
+  const html = Mustache.render(locationMessageTemplate, {
+    url,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  //disable form button to prevent accidental double clicks and sending same message twice
+  $messageFormButton.setAttribute("disabled", "disabled");
 
   const message = e.target.elements.message.value; //target represents target that we listen through the event on, and in this case it is the form, elements we have access to our elements property and we can access any of our inputs by their name, message.value gives out value of our input with name="message"
 
   //callback function is a event acknowledgement
   socket.emit("sendMessage", message, (error) => {
+    //enable send button
+    $messageFormButton.removeAttribute("disabled");
+
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -19,10 +53,12 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser.");
   }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
@@ -32,6 +68,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location shared");
       }
     );
